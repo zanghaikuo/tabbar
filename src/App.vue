3d0b172b0837f6b2a573ac3812dea7423b2d594f
zhk <798081009@qq.com>
2022-01-04 13:55:16 +0800

    333

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,20 @@
 <template>
   <div>
     <router-view></router-view>
-
-    <tab-bar>
-      <tab-bar-item path="/home" activeColor="#ff5777">
-        <template v-slot:icon>
-          <img src="./assets/img/tabbar/home.svg" alt="" />
-        </template>
-        <template v-slot:icon-active>
-          <img src="./assets/img/tabbar/home_active.svg" alt="" />
-        </template>
-        <template v-slot:text>
-          <p>首页</p>
-        </template>
-      </tab-bar-item>
-
-      <tab-bar-item path="/about">
-        <template v-slot:icon>
-          <img src="./assets/img/tabbar/category.svg" alt="" />
-        </template>
-        <template v-slot:icon-active>
-          <img src="./assets/img/tabbar/category_active.svg" alt="" />
-        </template>
-        <template v-slot:text>
-          <p>关于</p>
-        </template>
-      </tab-bar-item>
-
-      <tab-bar-item path="/cart">
-        <template v-slot:icon>
-          <img src="./assets/img/tabbar/shopcart.svg" alt="" />
-        </template>
-        <template v-slot:icon-active>
-          <img src="./assets/img/tabbar/shopcart_active.svg" alt="" />
-        </template>
-        <template v-slot:text>
-          <p>购物车</p>
-        </template>
-      </tab-bar-item>
-
-      <tab-bar-item path="/profile" activeColor="blue">
-        <template v-slot:icon>
-          <img src="./assets/img/tabbar/profile.svg" alt="" />
-        </template>
-        <template v-slot:icon-active>
-          <img src="./assets/img/tabbar/profile_active.svg" alt="" />
-        </template>
-        <template v-slot:text>
-          <p>我的</p>
-        </template>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
-import TabBar from "./views/TabBar.vue";
-import TabBarItem from "./views/TabBarItem.vue";
+import mainTabBar from './views/mainTabBar.vue'
+
 
 export default {
   name: "",
   methods: {},
   components: {
-    TabBar,
-    TabBarItem,
+    mainTabBar
+    
   },
 };
 </script>
---
+++ src/views/mainTabBar.vue
@@ -0,0 +1,72 @@
+<template>
+<tab-bar>
+      <tab-bar-item path="/home" activeColor="#ff5777">
+        <template v-slot:icon>
+          <img src="../assets/img/tabbar/home.svg" alt="" />
+        </template>
+        <template v-slot:icon-active>
+          <img src="../assets/img/tabbar/home_active.svg" alt="" />
+        </template>
+        <template v-slot:text>
+          <p>首页</p>
+        </template>
+      </tab-bar-item>
+
+      <tab-bar-item path="/about">
+        <template v-slot:icon>
+          <img src="../assets/img/tabbar/category.svg" alt="" />
+        </template>
+        <template v-slot:icon-active>
+          <img src="../assets/img/tabbar/category_active.svg" alt="" />
+        </template>
+        <template v-slot:text>
+          <p>关于</p>
+        </template>
+      </tab-bar-item>
+
+      <tab-bar-item path="/cart">
+        <template v-slot:icon>
+          <img src="../assets/img/tabbar/shopcart.svg" alt="" />
+        </template>
+        <template v-slot:icon-active>
+          <img src="../assets/img/tabbar/shopcart_active.svg" alt="" />
+        </template>
+        <template v-slot:text>
+          <p>购物车</p>
+        </template>
+      </tab-bar-item>
+
+      <tab-bar-item path="/profile" activeColor="blue">
+        <template v-slot:icon>
+          <img src="../assets/img/tabbar/profile.svg" alt="" />
+        </template>
+        <template v-slot:icon-active>
+          <img src="../assets/img/tabbar/profile_active.svg" alt="" />
+        </template>
+        <template v-slot:text>
+          <p>我的</p>
+        </template>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+import TabBar from "./TabBar.vue";
+import TabBarItem from "./TabBarItem.vue";
+export default {
+  name: '',
+  data(){
+    return {}
+  },
+  components:{
+      TabBar,
+      TabBarItem
+
+
+  }
+}
+</script>
+<style>
+
+
+</style>
